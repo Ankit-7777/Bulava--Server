33485da1e6db7b31f4da608dd74e88305f45a713
ankit-rvj-77 <ankit.kumawat@rvjtech.net>
2024-05-10 17:30:40 +0530

    database change

--- WeddingCard/settings.py
+++ WeddingCard/settings.py
@@ -81,17 +81,23 @@ WSGI_APPLICATION = 'WeddingCard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'Bulava',
+#         'USER':'root',
+#         'PASSWORD':'semi@123',
+#         'HOST':'localhost',
+#         'PORT':'3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'Bulava',
-        'USER':'root',
-        'PASSWORD':'semi@123',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
