26a0f049ec5a164a91213ea671d46f3b5d9c1d08
ankit-rvj-77 <ankit.kumawat@rvjtech.net>
2024-05-10 22:50:19 +0530

    semurl

--- WeddingCard/urls.py
+++ WeddingCard/urls.py
@@ -7,18 +7,7 @@ from WeddingApp.views import (
     CoverImageViewSet,
     EventViewSet,
     ContectUsViewSet,
-    UserRegistrationView,
-    UserLoginView,
-    UserUpdateView,
-    UserChangePasswordView,
-    SendPasswordResetEmailView,
-    UserPasswordResetView,
-    LogoutUserView,
-    UserProfileView,
-    AllUserProfileView,
 )
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -33,23 +22,7 @@ router.register(r'contect-us', ContectUsViewSet, basename='contect-us')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apis/', include('WeddingApp.urls')),
+    path('', include('WeddingApp.urls')),
     path('auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
-    path('user-registration/', UserRegistrationView.as_view(), name='register-UserProfile'),
-    path('user-login/', UserLoginView.as_view(), name='login-UserProfile'),
-    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
-    path('user-update/<int:pk>/', UserUpdateView.as_view(), name='UserProfile-update'),
-    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
-    path('user-logout/', LogoutUserView.as_view(), name='user-logout'),
-    path('users/', AllUserProfileView.as_view(), name='user_profile_list'),
-    path('users/<int:pk>/', AllUserProfileView.as_view(), name='user_profile_detail'),
-    path('gettoken/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('profile/', UserProfileView.as_view(), name='profile'), 
+    path('', include(router.urls)), 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
