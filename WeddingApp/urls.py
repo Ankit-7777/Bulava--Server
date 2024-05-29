bed34a3fd998b005f39874b4686fcf84c946a031
ankit-rvj-77 <ankit.kumawat@rvjtech.net>
2024-05-29 12:31:45 +0530

    changes in the setting and urls

--- WeddingApp/urls.py
+++ WeddingApp/urls.py
@@ -7,7 +7,6 @@ from WeddingApp.views import (
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
--- WeddingCard/urls.py
+++ WeddingCard/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from WeddingApp import views
 from rest_framework.routers import DefaultRouter
 from WeddingApp.views import (
     CategoryViewSet,
