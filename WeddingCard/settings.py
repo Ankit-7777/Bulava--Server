513d044a0fe3cf4c93a7b223a5fb645311f8991b
toshiladitya <adityasharmanimod@gmail.com>
2024-05-31 15:34:48 +0530

    chnn

--- WeddingCard/settings.py
+++ WeddingCard/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*c+awqyrp-yv@67*3_@qs^m#w@32*ua%p0jk*+yjl9-wx9v%1f')
+SECRET_KEY = 'django-insecure-*c+awqyrp-yv@67*3_@qs^m#w@32*ua%p0jk*+yjl9-wx9v%1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
