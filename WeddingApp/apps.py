2245fdcd5535a4e33f3db3bc02faf48a6ad3f0e4
ankit-7777 <ankitralawata20218@gmail.com>
2024-06-07 16:50:21 +0530

    exclude signals

--- WeddingApp/apps.py
+++ WeddingApp/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class WeddingappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'WeddingApp'
-    def ready(self):
-        import WeddingApp.signals
+    # def ready(self):
+    #     import WeddingApp.signals
