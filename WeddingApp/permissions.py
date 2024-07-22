c2bfc550e68d45b0e29a17c1d8cea75c0ed1bcd6
Gatik-Ankit-7777 <gatikai.ankit@gmail.com>
2024-07-22 10:33:42 +0530

    modified event public event permisiions

--- WeddingApp/permissions.py
+++ WeddingApp/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from WeddingApp.models import Event
+from django.core.exceptions import ObjectDoesNotExist
 
 class IsSuperuserOrReadOnly(BasePermission):
     """
@@ -15,12 +16,18 @@ class IsSuperuserOrReadOnly(BasePermission):
                 return True
         return False
 
-class IsEventOwner(BasePermission):
-    """
-    Custom permission to only allow owners of an event to edit or delete it.
-    """
-    def has_object_permission(self, request, view, obj):
+class EventPermission(BasePermission):
+    def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            if not obj.is_private:
+                return True
+            if request.user and request.user.is_authenticated:
+                return obj.user == request.user or obj.invited.filter(id=request.user.id).exists()
+            return False
         return obj.user == request.user
 
